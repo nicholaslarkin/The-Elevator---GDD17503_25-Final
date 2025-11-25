bb617aee2bbb9727aa136cb9679d26a3ed27d9ef
nicholaslarkin <nglarkin@quinnipiac.edu>
2025-11-25 12:01:56 -0500

    little code

---
+++ Assets/-SCRIPTS/CharacterRandomizer.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class CharacterRandomizer : MonoBehaviour
+{
+    [Header("Freak State")]
+    [SerializeField] private bool freakInElevator = false;
+
+    [Header("Values")]
+    [SerializeField] private int skinColor;
+    [SerializeField] private int hatType;
+    [SerializeField] private int personality;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Randomize();
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            freakInElevator = true;
+        }
+    }
+
+    private void Randomize()
+    {
+        if (freakInElevator)
+        {
+            return;
+        }
+        skinColor = Random.Range(0, 6);
+        hatType = Random.Range(0, 3);
+        personality = Random.Range(0, 3);
+    }
+}
