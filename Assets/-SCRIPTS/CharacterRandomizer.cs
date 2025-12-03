e0ae32a1dc96a7a4cee37dbd35457dedb9290c74
Nicholas Larkin <nglarkin@quinnipiac.edu>
2025-12-03 06:58:29 -0500

    Revert "Merge pull request #8 from nicholaslarkin/Nickybranch"
    This reverts commit 9b2dfe481543d4213d6f5ad6f2d36d3592c1a914, reversing
    changes made to e53ab6ef6feb59cef75c7d2f0c1d2d6c9eda64e2.

--- Assets/-SCRIPTS/CharacterRandomizer.cs
+++ Assets/-SCRIPTS/CharacterRandomizer.cs
@@ -1,31 +1,32 @@
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class CharacterRandomizer : MonoBehaviour
 {
-    public Elevator elevator;
-
     [Header("Freak State")]
     [SerializeField] public bool freakInElevator = false;
-    [SerializeField] public int requestFloor;
 
     [Header("Values")]
-    [SerializeField] private Color skinColor;
-    [SerializeField] private Color mouthColor;
-    [SerializeField] private Color earColor;
-    [SerializeField] private int voiceType;
+    [SerializeField] private int skinColor;
+    [SerializeField] private int hatType;
     [SerializeField] private int personality;
 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R)) //***TEST INPUT; CHANGE LATER***
+        if (Input.GetKeyDown(KeyCode.R))
         {
             Randomize();
 
-            AudioManager.instance.PlayOneShot(FMODEvents.instance.buttonPressed, this.transform.position); //test sound; runs from FMOD hardcoded into script
+            AudioManager.instance.PlayOneShot(FMODEvents.instance.buttonPressed, this.transform.position); //test sound
         }
 
-        if (Input.GetKeyDown(KeyCode.T)) //***TEST INPUT; CHANGE LATER***
+        if (Input.GetKeyDown(KeyCode.T))
         {
             freakInElevator = true;
         }
@@ -37,15 +38,8 @@ public class CharacterRandomizer : MonoBehaviour
         {
             return;
         }
-
-        ///SCALABLE PARAMETERS///
-        skinColor = Random.ColorHSV(0f, 1f, 0.8f, 1f, 0.7f, 1f); //***change all the values to work with RGB***//
-        mouthColor = Random.ColorHSV(0f, 1f, 0.8f, 1f, 0.7f, 1f);
-        earColor = Random.ColorHSV(0f, 1f, 0.8f, 1f, 0.7f, 1f);
-
-        voiceType = Random.Range(0, 3); //***change range later based on amount of voices***//
+        skinColor = Random.Range(0, 6);
+        hatType = Random.Range(0, 3);
         personality = Random.Range(0, 3);
-
-        requestFloor = Random.Range(0, elevator.currentFloor); //calls a random number to be selected as the requested floor
     }
 }
